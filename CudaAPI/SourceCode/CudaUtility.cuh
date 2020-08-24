3611f810c57ce60e0a0a67ba0af5d7de14f50009
hanshubin <hanshubin@xd.com>
2020-08-24 22:32:36 +0800

    Tried cuda assert.

---
+++ CudaAPI/SourceCode/CudaUtility.cuh
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <cuda_runtime.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cuda/std/cassert>
+#include <device_launch_parameters.h>
+
+#define CHECK(isAssert, errorInfo) check(isAssert, errorInfo, __FILE__, __LINE__)
+__device__ void check(const bool& isAssert, const char* errorInfo, const char* file, const int& line)
+{
+	if (!isAssert)
+	{
+		printf("====================Error Occurred!====================\n");
+		printf("Error occurred in: %s\n", file);
+		printf("Line: %d\n", line);
+		printf("Error info:%s\n", errorInfo);
+		printf("====================Error Occurred!====================\n");
+		assert(false);
+	}
+}
+
+#define CUDA_CHECK(err) do { error_check(err, __FILE__, __LINE__); } while(0)
+inline void error_check(cudaError_t err, const char* file, int line) {
+	if (err != cudaSuccess) {
+		::fprintf(stderr, "CUDA ERROR at %s[%d] : %s\n", file, line, cudaGetErrorString(err));
+		abort();
+	}
+}
--- CudaAPI/SourceCode/kernel.cu
+++ CudaAPI/SourceCode/kernel.cu
@@ -1,35 +1,23 @@
-﻿#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
+﻿#include "CudaUtility.cuh"
+
+//Referenced: https://stackoverflow.com/questions/50755717/triggering-a-runtime-error-within-a-cuda-kernel
 
 __global__ void kernel()
 {
-	auto i = 11;
-	auto op = i + 12.0f;
-	//thrust::device_vector<int> vec;
-	//vec.push_back(op);
-	//int temp = vec[0];
-	//printf("%d", temp);
-	//thrust library can not use in kernel.
-	//Referenced: https://forums.developer.nvidia.com/t/using-std-vector-in-cuda-kernel-its-posible-to-use-a-std-vector-inside-cuda-kernel/16307/3
-	//Referenced: https://docs.nvidia.com/cuda/thrust/index.html
+	//If paramater is true, nothing would happen.
+	assert(1);
+	
+	//This function can not use. I don't know why.
+	//asm("trap\n");
+	CHECK(false, "Called this check.");
+	printf("Yes\n");
 }
 
 int main()
 {
-	thrust::host_vector<int> H(4);
 
-	H[0] = 14;
-	H[1] = 20;
-	H[2] = 38;
-	H[3] = 46;
-
-	std::cout << "H has size: " << H.size() << std::endl;
-
-	for each (auto var in H)
-	{
-		std::cout << var << std::endl;
-	}
-	thrust::device_vector<int> device_vec;
-	device_vec = H;
 	kernel << <1, 1>> > ();
+	//CUDA_CHECK(cudaGetLastError());
+	//CUDA_CHECK(cudaDeviceSynchronize());
+
 }
