66555b8e4d7d8a3f583ae200a3f5b294dbf83282
hanshubin <hanshubin@xd.com>
2020-08-24 21:11:29 +0800

    Try STD in CUDA kernel.
    Summary: STD can not use ing CUDA kernel.
    Referenced: https://forums.developer.nvidia.com/t/using-std-vector-in-cuda-kernel-its-posible-to-use-a-std-vector-inside-cuda-kernel/16307/3
     Referenced: https://docs.nvidia.com/cuda/thrust/index.html

--- CudaAPI/SourceCode/kernel.cu
+++ CudaAPI/SourceCode/kernel.cu
@@ -1,78 +1,35 @@
-﻿#include <stdio.h>
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <cuda/std/cassert>
-namespace std
-{
-	template<typename T>
-	class vector
-	{
-	private:
-		T* ptr;
-		unsigned int size;
-	public:
-		__device__ vector() : size(0)
-		{
-			printf("Called vector()\n");
-		}
-		__device__ ~vector()
-		{
-			printf("Called ~vector()\n");
-			if (ptr)
-				delete[] ptr;
-		}
-
-		__device__ void push_back(const T& t)
-		{
-			printf("Called push_back()\n");
-			unsigned int newSize = size + 1;
-			T* newPtr = new T[newSize];
-			for (unsigned int i = 0; i < size; i++)
-			{
-				printf("ptr[%d] is %d", i, ptr[i]);
-				newPtr[i] = ptr[i];
-			}
-			newPtr[size] = t;
-			size = newSize;
-		}
-		__device__ unsigned int Size()
-		{
-			printf("Called Size()\n");
-			return size;
-		}
-	};
-}
-
-template <typename T>
-class Foo
-{
-private:
-	T val = 0;
-public:
-	__device__ Foo()
-	{
-		printf("Foo::Foo() is called\n");
-	}
-	__device__ virtual ~Foo()
-	{
-		printf("Foo::~Foo() is called\n");
-	}
-
-	__device__ virtual void show() noexcept
-	{
-		printf("%f",val);
-	}
-};
+﻿#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
 __global__ void kernel()
 {
-	std::vector<int> test;
-	int size = test.Size();
-	test.push_back(2);
-	
+	auto i = 11;
+	auto op = i + 12.0f;
+	//thrust::device_vector<int> vec;
+	//vec.push_back(op);
+	//int temp = vec[0];
+	//printf("%d", temp);
+	//thrust library can not use in kernel.
+	//Referenced: https://forums.developer.nvidia.com/t/using-std-vector-in-cuda-kernel-its-posible-to-use-a-std-vector-inside-cuda-kernel/16307/3
+	//Referenced: https://docs.nvidia.com/cuda/thrust/index.html
 }
 
 int main()
 {
+	thrust::host_vector<int> H(4);
+
+	H[0] = 14;
+	H[1] = 20;
+	H[2] = 38;
+	H[3] = 46;
+
+	std::cout << "H has size: " << H.size() << std::endl;
+
+	for each (auto var in H)
+	{
+		std::cout << var << std::endl;
+	}
+	thrust::device_vector<int> device_vec;
+	device_vec = H;
 	kernel << <1, 1>> > ();
 }
