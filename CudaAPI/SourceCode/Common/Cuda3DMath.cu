4006eb8402f410083481aab969264d5c5ceb79b4
HorribleCavalry <HordrenHan@outlook.com>
2020-08-31 02:10:53 +0800

    Added vec3.

--- CudaAPI/SourceCode/Common/Cuda3DMath.cu
+++ CudaAPI/SourceCode/Common/Cuda3DMath.cu
@@ -165,30 +165,30 @@ namespace CUM
 
 	__duel__ const vec2<Int> operator/(const Int & n, const vec2<Int>& v)
 	{
-		CHECK(v.x != 0, "(Int)n/vec2<int>(v).x: can not divide 0!");
-		CHECK(v.y != 0, "(Int)n/vec2<int>(v).y: can not divide 0!");
+		CHECK(v.x != 0, "(Int)n/vec2<Int>(v).x: can not divide 0!");
+		CHECK(v.y != 0, "(Int)n/vec2<Int>(v).y: can not divide 0!");
 
 		return vec2<Int>(n / v.x, n / v.y);
 	}
 
 	__duel__ const vec2<Int> operator/(const vec2<Int>& v, const Int & n)
 	{
-		CHECK(n != 0, "vec2<int>(v)/n: can not divide 0!");
+		CHECK(n != 0, "vec2<Int>(v)/n: can not divide 0!");
 
 		return vec2<Int>(v.x / n, v.y / n);
 	}
 
 	__duel__ const vec2<Int> operator/(const vec2<Int>& v0, const vec2<Int>& v1)
 	{
-		CHECK(v1.x != 0, "vec2<int>(v0).x/vec2<int>(v1).x: can not divide 0!");
-		CHECK(v1.y != 0, "vec2<int>(v0).y/vec2<int>(v1).y: can not divide 0!");
+		CHECK(v1.x != 0, "vec2<Int>(v0).x/vec2<Int>(v1).x: can not divide 0!");
+		CHECK(v1.y != 0, "vec2<Int>(v0).y/vec2<Int>(v1).y: can not divide 0!");
 
 		return vec2<Int>(v0.x / v1.x, v0.y / v1.y);
 	}
 
 	__duel__ const vec2<Int>& operator/=(vec2<Int>& v, const Int & n)
 	{
-		CHECK(n != 0, "vec2<int>(v).y/=(int)n: can not divide 0!");
+		CHECK(n != 0, "vec2<Int>(v).y/=(Int)n: can not divide 0!");
 
 		v.x /= n;
 		v.y /= n;
@@ -197,8 +197,8 @@ namespace CUM
 
 	__duel__ const vec2<Int>& operator/=(vec2<Int>& v0, const vec2<Int>& v1)
 	{
-		CHECK(v1.x != 0, "vec2<int>(v0).x/=vec2<int>(v1).x: can not divide 0!");
-		CHECK(v1.y != 0, "vec2<int>(v0).y/=vec2<int>(v1).y: can not divide 0!");
+		CHECK(v1.x != 0, "vec2<Int>(v0).x/=vec2<Int>(v1).x: can not divide 0!");
+		CHECK(v1.y != 0, "vec2<Int>(v0).y/=vec2<Int>(v1).y: can not divide 0!");
 
 		v0.x /= v1.x;
 		v0.y /= v1.y;
@@ -417,6 +417,455 @@ namespace CUM
 
 		v0.x /= v1.x;
 		v0.y /= v1.y;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma endregion
+
+#pragma region vec3
+
+#pragma region vec3 dot operation
+
+	const Int dot(const vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		return v0.x*v1.x + v0.y*v1.y;
+	}
+	const Float dot(const vec3<Int>& v0, const vec3<Float>& v1)
+	{
+		return v0.x*v1.x + v0.y*v1.y;
+	}
+	const Float dot(const vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		return v0.x*v1.x + v0.y*v1.y;
+	}
+	const Float dot(const vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		return v0.x*v1.x + v0.y*v1.y;
+	}
+
+	const Int dot(const Int& n, const vec3<Int>& v)
+	{
+		return n * v.x + n * v.y;
+	}
+	const Float dot(const Int& n, const vec3<Float>& v)
+	{
+		return n * v.x + n * v.y;
+	}
+	const Float dot(const Float& n, const vec3<Int>& v)
+	{
+		return n * v.x + n * v.y;
+	}
+	const Float dot(const Float& n, const vec3<Float>& v)
+	{
+		return n * v.x + n * v.y;
+	}
+
+	const Int dot(const vec3<Int>& v, const Int& n)
+	{
+		return v.x*n + v.y*n;
+	}
+
+	const Float dot(const vec3<Int>& v, const Float& n)
+	{
+		return v.x*n + v.y*n;
+	}
+
+	const Float dot(const vec3<Float>& v, const Int& n)
+	{
+		return v.x*n + v.y*n;
+	}
+
+	const Float dot(const vec3<Float>& v, const Float& n)
+	{
+		return v.x*n + v.y*n;
+	}
+
+#pragma endregion
+
+#pragma region vec3i add operation
+
+	__duel__ const vec3<Int> operator+(const Int & n, const vec3<Int>& v)
+	{
+		return vec3<Int>(n + v.x, n + v.y, n+v.z);
+	}
+
+	__duel__ const vec3<Int> operator+(const vec3<Int>& v, const Int & n)
+	{
+		return vec3<Int>(v.x + n, v.y + n, v.z + n);
+	}
+
+	__duel__ const vec3<Int> operator+(const vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Int>(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
+	}
+
+	__duel__ const vec3<Int>& operator+=(vec3<Int>& v, const Int & n)
+	{
+		v.x += n;
+		v.y += n;
+		return v;
+	}
+
+	__duel__ const vec3<Int>& operator+=(vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		v0.x += v1.x;
+		v0.y += v1.y;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3i subtract operation
+
+	__duel__ const vec3<Int> operator-(const Int & n, const vec3<Int>& v)
+	{
+		return vec3<Int>(n - v.x, n - v.y, n - v.z);
+	}
+
+	__duel__ const vec3<Int> operator-(const vec3<Int>& v, const Int & n)
+	{
+		return vec3<Int>(v.x - n, v.y - n, v.z - n);
+	}
+
+	__duel__ const vec3<Int> operator-(const vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Int>(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+	}
+
+	__duel__ const vec3<Int>& operator-=(vec3<Int>& v, const Int & n)
+	{
+		v.x -= n;
+		v.y -= n;
+		v.z -= n;
+		return v;
+	}
+
+	__duel__ const vec3<Int>& operator-=(vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		v0.x -= v1.x;
+		v0.y -= v1.y;
+		v0.z -= v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3i multiply operation
+
+	__duel__ const vec3<Int> operator*(const Int & n, const vec3<Int>& v)
+	{
+		return vec3<Int>(n * v.x, n * v.y, n * v.z);
+	}
+
+	__duel__ const vec3<Int> operator*(const vec3<Int>& v, const Int & n)
+	{
+		return vec3<Int>(v.x * n, v.y * n, v.z * n);
+	}
+
+	__duel__ const vec3<Int> operator*(const vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Int>(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
+	}
+
+	__duel__ const vec3<Int>& operator*=(vec3<Int>& v, const Int & n)
+	{
+		v.x *= n;
+		v.y *= n;
+		v.z *= n;
+		return v;
+	}
+
+	__duel__ const vec3<Int>& operator*=(vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		v0.x *= v1.x;
+		v0.y *= v1.y;
+		v0.z *= v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3i divide operation
+
+	__duel__ const vec3<Int> operator/(const Int & n, const vec3<Int>& v)
+	{
+		CHECK(v.x != 0, "(Int)n/vec3<Int>(v).x: can not divide 0!");
+		CHECK(v.y != 0, "(Int)n/vec3<Int>(v).y: can not divide 0!");
+		CHECK(v.z != 0, "(Int)n/vec3<Int>(v).z: can not divide 0!");
+
+		return vec3<Int>(n / v.x, n / v.y, n/v.z);
+	}
+
+	__duel__ const vec3<Int> operator/(const vec3<Int>& v, const Int & n)
+	{
+		CHECK(n != 0, "vec3<Int>(v)/n: can not divide 0!");
+
+		return vec3<Int>(v.x / n, v.y / n, v.z / n);
+	}
+
+	__duel__ const vec3<Int> operator/(const vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Int>(v0).x/vec3<Int>(v1).x: can not divide 0!");
+		CHECK(v1.y != 0, "vec3<Int>(v0).y/vec3<Int>(v1).y: can not divide 0!");
+		CHECK(v1.z != 0, "vec3<Int>(v0).y/vec3<Int>(v1).z: can not divide 0!");
+
+		return vec3<Int>(v0.x / v1.x, v0.y / v1.y, v0.z / v1.z);
+	}
+
+	__duel__ const vec3<Int>& operator/=(vec3<Int>& v, const Int & n)
+	{
+		CHECK(n != 0, "vec3<Int>(v)/=(Int)n: can not divide 0!");
+
+		v.x /= n;
+		v.y /= n;
+		v.z /= n;
+		return v;
+	}
+
+	__duel__ const vec3<Int>& operator/=(vec3<Int>& v0, const vec3<Int>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Int>(v0).x/=vec3<Int>(v1).x: can not divide 0!");
+		CHECK(v1.y != 0, "vec3<Int>(v0).y/=vec3<Int>(v1).y: can not divide 0!");
+		CHECK(v1.z != 0, "vec3<Int>(v0).z/=vec3<Int>(v1).z: can not divide 0!");
+
+		v0.x /= v1.x;
+		v0.y /= v1.y;
+		v0.z /= v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+
+#pragma region vec3f add operation
+
+	__duel__ const vec3<Float> operator+(const Float& n, const vec3<Float>& v)
+	{
+		return vec3<Float>(n + v.x, n + v.y, n + v.z);
+	}
+
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v, const Float& n)
+	{
+		return vec3<Float>(v.x + n, v.y + n, v.z + n);
+	}
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Float>(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
+	}
+
+	__duel__ const vec3<Float> operator+(const vec3<Int>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
+	}
+
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z);
+	}
+
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v, const Float& n)
+	{
+		v.x += n;
+		v.y += n;
+		v.z += n;
+		return v;
+	}
+
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		v0.x += v1.x;
+		v0.y += v1.y;
+		v0.z += v1.z;
+		return v0;
+	}
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		v0.x += v1.x;
+		v0.y += v1.y;
+		v0.z += v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3f subtract operation
+
+	__duel__ const vec3<Float> operator-(const Float& n, const vec3<Float>& v)
+	{
+		return vec3<Float>(n - v.x, n - v.y, n - v.z);
+	}
+
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v, const Float& n)
+	{
+		return vec3<Float>(v.x - n, v.y - n, v.z - n);
+	}
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Float>(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+	}
+
+	__duel__ const vec3<Float> operator-(const vec3<Int>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+	}
+
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z);
+	}
+
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v, const Float& n)
+	{
+		v.x -= n;
+		v.y -= n;
+		v.z -= n;
+		return v;
+	}
+
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		v0.x -= v1.x;
+		v0.y -= v1.y;
+		v0.z -= v1.z;
+		return v0;
+	}
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		v0.x -= v1.x;
+		v0.y -= v1.y;
+		v0.z -= v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3f multiply operation
+
+	__duel__ const vec3<Float> operator*(const Float& n, const vec3<Float>& v)
+	{
+		return vec3<Float>(n * v.x, n * v.y, n * v.z);
+	}
+
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v, const Float& n)
+	{
+		return vec3<Float>(v.x * n, v.y * n, v.z * n);
+	}
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		return vec3<Float>(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
+	}
+
+	__duel__ const vec3<Float> operator*(const vec3<Int>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
+	}
+
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		return vec3<Float>(v0.x * v1.x, v0.y * v1.y, v0.z * v1.z);
+	}
+
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v, const Float& n)
+	{
+		v.x *= n;
+		v.y *= n;
+		v.z *= n;
+		return v;
+	}
+
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		v0.x *= v1.x;
+		v0.y *= v1.y;
+		v0.z *= v1.z;
+		return v0;
+	}
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		v0.x *= v1.x;
+		v0.y *= v1.y;
+		v0.z *= v1.z;
+		return v0;
+	}
+
+#pragma endregion
+
+#pragma region vec3f divide operation
+
+	__duel__ const vec3<Float> operator/(const Float& n, const vec3<Float>& v)
+	{
+		CHECK(v.x != 0, "(Float)n/vec3<Float>(v).x: can not devide 0!");
+		CHECK(v.y != 0, "(Float)n/vec3<Float>(v).y: can not devide 0!");
+		CHECK(v.z != 0, "(Float)n/vec3<Float>(v).z: can not devide 0!");
+
+		return vec3<Float>(n / v.x, n / v.y, n / v.z);
+	}
+
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v, const Float& n)
+	{
+		CHECK(n != 0, "vec3<Float>(v)/(Float)n: can not devide 0!");
+
+		return vec3<Float>(v.x / n, v.y / n, v.z / n);
+	}
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Float>(v0).x/vec3<Int>(v1).x: can not devide 0!");
+		CHECK(v1.y != 0, "vec3<Float>(v0).y/vec3<Int>(v1).y: can not devide 0!");
+		CHECK(v1.z != 0, "vec3<Float>(v0).z/vec3<Int>(v1).z: can not devide 0!");
+
+		return vec3<Float>(v0.x / v1.x, v0.y / v1.y, v0.z / v1.z);
+	}
+
+	__duel__ const vec3<Float> operator/(const vec3<Int>& v0, const vec3<Float>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Int>(v0).x/vec3<Float>(v1).x: can not devide 0!");
+		CHECK(v1.y != 0, "vec3<Int>(v0).y/vec3<Float>(v1).y: can not devide 0!");
+		CHECK(v1.z != 0, "vec3<Int>(v0).z/vec3<Float>(v1).z: can not devide 0!");
+
+		return vec3<Float>(v0.x / v1.x, v0.y / v1.y, v0.z / v1.z);
+	}
+
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Float>(v0).x/vec3<Float>(v1).x: can not devide 0!");
+		CHECK(v1.y != 0, "vec3<Float>(v0).y/vec3<Float>(v1).y: can not devide 0!");
+		CHECK(v1.z != 0, "vec3<Float>(v0).z/vec3<Float>(v1).z: can not devide 0!");
+
+		return vec3<Float>(v0.x / v1.x, v0.y / v1.y, v0.z / v1.z);
+	}
+
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v, const Float& n)
+	{
+		CHECK(n != 0, "vec3<Float>(v)/=(Float)n: can not devide 0!");
+
+		v.x /= n;
+		v.y /= n;
+		v.z /= n;
+		return v;
+	}
+
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v0, const vec3<Int>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Float>(v0).x/=vec3<Int>(v1).x: can not devide 0!");
+		CHECK(v1.y != 0, "vec3<Float>(v0).y/=vec3<Int>(v1).y: can not devide 0!");
+		CHECK(v1.z != 0, "vec3<Float>(v0).z/=vec3<Int>(v1).z: can not devide 0!");
+
+		v0.x /= v1.x;
+		v0.y /= v1.y;
+		v0.z /= v1.z;
+		return v0;
+	}
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v0, const vec3<Float>& v1)
+	{
+		CHECK(v1.x != 0, "vec3<Float>(v0).x/=vec3<Float>(v1).x: can not devide 0!");
+		CHECK(v1.y != 0, "vec3<Float>(v0).y/=vec3<Float>(v1).y: can not devide 0!");
+		CHECK(v1.z != 0, "vec3<Float>(v0).z/=vec3<Float>(v1).z: can not devide 0!");
+
+		v0.x /= v1.x;
+		v0.y /= v1.y;
+		v0.z /= v1.z;
 		return v0;
 	}
 
--- CudaAPI/SourceCode/Common/Cuda3DMath.cuh
+++ CudaAPI/SourceCode/Common/Cuda3DMath.cuh
@@ -224,6 +224,244 @@ namespace CUM
 
 #pragma endregion
 
+#pragma region vec3
+	template<typename T>
+	class vec3
+	{
+	public:
+		T x, y, z;
+	public:
+		__duel__ vec3() :x(0.0), y(0.0), z(0.0) {}
+		__duel__ vec3(const vec3<T>& v) : x(v.x), y(v.y), z(v.z) {}
+		__duel__ vec3(vec3<T>&& v) : x(v.x), y(v.y), z(v.z) {}
+		__duel__ vec3<T>& operator=(const vec3<T>& v)
+		{
+			x = v.x;
+			y = v.y;
+			z = v.z;
+			return *this;
+		}
+		__duel__ vec3<T>& operator=(vec3<T>&& v)
+		{
+			x = v.x;
+			y = v.y;
+			z = v.z;
+			return *this;
+		}
+		__duel__ ~vec3() {}
+	public:
+		__duel__ vec3(const T& n) : x(n), y(n), z(n) {}
+		__duel__ vec3(const T& _x, const T& _y, const T& _z) : x(_x), y(_y), z(_z) {}
+
+		template<typename U>
+		__duel__ vec3<T>(const vec3<U>& v) : x(v.x), y(v.y), z(v.z) {}
+
+	public:
+		__duel__ T& operator[](const T& idx)
+		{
+			CHECK(idx >= 0 && idx <= 2, "The <idx> in vec3<T>::operator[idx] is illegal!");
+			switch (idx)
+			{
+			case 0: return x; break;
+			case 1: return y; break;
+			case 2: return z; break;
+			default:
+				CHECK(false, "Unexpectedly call this T& vec3<T>::operator[](const T& idx), unscientific!");
+				break;
+			}
+		}
+	};
+
+	template<>
+	class vec3<Int>
+	{
+	public:
+		Int x, y, z;
+	public:
+		__duel__ vec3() :x(0), y(0), z(0) {}
+		__duel__ vec3(const vec3<Int>& v) : x(v.x), y(v.y), z(v.z) {}
+		__duel__ vec3(vec3<Int>&& v) : x(v.x), y(v.y), z(v.z) {}
+		__duel__ const vec3<Int>& operator=(const vec3<Int>& v)
+		{
+			x = v.x;
+			y = v.y;
+			z = v.z;
+			return *this;
+		}
+		__duel__ const vec3<Int>& operator=(vec3<Int>&& v)
+		{
+			x = v.x;
+			y = v.y;
+			z = v.z;
+			return *this;
+		}
+		__duel__ ~vec3() {}
+	public:
+		__duel__ vec3(const Int& n) : x(n), y(n), z(n) {}
+		__duel__ vec3(const Int& _x, const Int& _y, const Int& _z) : x(_x), y(_y), z(_z) {}
+
+		__duel__ vec3(const Float& n) = delete;
+		__duel__ vec3(const Float& _x, const Float& _y, const Float& _z) = delete;
+
+		__duel__ explicit vec3(const vec3<Float>& v) : x(v.x), y(v.y), z(v.z) {}
+
+		__duel__ const vec3<Int>& operator=(const Int& n)
+		{
+			x = n;
+			y = n;
+			z = n;
+			return *this;
+		}
+		__duel__ const vec3<Int>& operator=(Int&& n)
+		{
+			x = n;
+			y = n;
+			z = n;
+			return *this;
+		}
+
+		__duel__ const vec3<Int>& operator=(const Float& n) = delete;
+		__duel__ const vec3<Int>& operator=(Float&& n) = delete;
+	public:
+		__duel__ Int& operator[](const Int& idx)
+		{
+			CHECK(idx >= 0 && idx <= 2, "The <idx> in vec3<T>::operator[idx] is illegal!");
+			switch (idx)
+			{
+			case 0: return x; break;
+			case 1: return y; break;
+			case 2: return x; break;
+			default:
+				CHECK(false, "Unexpectedly call this T& vec3<T>::operator[](const T& idx), unscientific!");
+				break;
+			}
+		}
+	};
+
+	typedef vec3<Int> vec3i;
+	typedef vec3<Float> vec3f;
+
+#pragma region vec3 dot operation
+
+	const Int dot(const vec3<Int>& v0, const vec3<Int>& v1);
+	const Float dot(const vec3<Int>& v0, const vec3<Float>& v1);
+	const Float dot(const vec3<Float>& v0, const vec3<Int>& v1);
+	const Float dot(const vec3<Float>& v0, const vec3<Float>& v1);
+
+	const Int dot(const Int& n, const vec3<Int>& v);
+	const Float dot(const Int& n, const vec3<Float>& v);
+	const Float dot(const Float& n, const vec3<Int>& v);
+	const Float dot(const Float& n, const vec3<Float>& v);
+
+	const Int dot(const vec3<Int>& v, const Int& n);
+	const Float dot(const vec3<Int>& v, const Float& n);
+	const Float dot(const vec3<Float>& v, const Int& n);
+	const Float dot(const vec3<Float>& v, const Float& n);
+
+#pragma endregion
+
+#pragma region vec3i add operation
+
+	__duel__ const vec3<Int> operator+(const Int& n, const vec3<Int>& v);
+	__duel__ const vec3<Int> operator+(const vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int> operator+(const vec3<Int>& v0, const vec3<Int>& v1);
+
+	__duel__ const vec3<Int>& operator+=(vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int>& operator+=(vec3<Int>& v0, const vec3<Int>& v1);
+
+#pragma endregion
+
+#pragma region vec3i subtract operation
+
+	__duel__ const vec3<Int> operator-(const Int& n, const vec3<Int>& v);
+	__duel__ const vec3<Int> operator-(const vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int> operator-(const vec3<Int>& v0, const vec3<Int>& v1);
+
+	__duel__ const vec3<Int>& operator-=(vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int>& operator-=(vec3<Int>& v0, const vec3<Int>& v1);
+
+#pragma endregion
+
+#pragma region vec3i multiply operation
+
+	__duel__ const vec3<Int> operator*(const Int& n, const vec3<Int>& v);
+	__duel__ const vec3<Int> operator*(const vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int> operator*(const vec3<Int>& v0, const vec3<Int>& v1);
+
+	__duel__ const vec3<Int>& operator*=(vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int>& operator*=(vec3<Int>& v0, const vec3<Int>& v1);
+
+#pragma endregion
+
+#pragma region vec3i divide operation
+
+	__duel__ const vec3<Int> operator/(const Int& n, const vec3<Int>& v);
+	__duel__ const vec3<Int> operator/(const vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int> operator/(const vec3<Int>& v0, const vec3<Int>& v1);
+
+	__duel__ const vec3<Int>& operator/=(vec3<Int>& v, const Int& n);
+	__duel__ const vec3<Int>& operator/=(vec3<Int>& v0, const vec3<Int>& v1);
+
+#pragma endregion
+
+#pragma region vec3f add operation
+
+	__duel__ const vec3<Float> operator+(const Float& n, const vec3<Float>& v);
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float> operator+(const vec3<Int>& v0, const vec3<Float>& v1);
+	__duel__ const vec3<Float> operator+(const vec3<Float>& v0, const vec3<Float>& v1);
+
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float>& operator+=(vec3<Float>& v0, const vec3<Float>& v1);
+
+#pragma endregion
+
+#pragma region vec3f subtract operation
+
+	__duel__ const vec3<Float> operator-(const Float& n, const vec3<Float>& v);
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float> operator-(const vec3<Int>& v0, const vec3<Float>& v1);
+	__duel__ const vec3<Float> operator-(const vec3<Float>& v0, const vec3<Float>& v1);
+
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float>& operator-=(vec3<Float>& v0, const vec3<Float>& v1);
+
+#pragma endregion
+
+#pragma region vec3f multiply operation
+
+	__duel__ const vec3<Float> operator*(const Float& n, const vec3<Float>& v);
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float> operator*(const vec3<Int>& v0, const vec3<Float>& v1);
+	__duel__ const vec3<Float> operator*(const vec3<Float>& v0, const vec3<Float>& v1);
+
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float>& operator*=(vec3<Float>& v0, const vec3<Float>& v1);
+
+#pragma endregion
+
+#pragma region vec3f divide operation
+
+	__duel__ const vec3<Float> operator/(const Float& n, const vec3<Float>& v);
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float> operator/(const vec3<Int>& v0, const vec3<Float>& v1);
+	__duel__ const vec3<Float> operator/(const vec3<Float>& v0, const vec3<Float>& v1);
+
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v, const Float& n);
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v0, const vec3<Int>& v1);
+	__duel__ const vec3<Float>& operator/=(vec3<Float>& v0, const vec3<Float>& v1);
+
+#pragma endregion
+
+#pragma endregion
+
 }
 
 #endif // !__CUDA3DMATH__CUH__
